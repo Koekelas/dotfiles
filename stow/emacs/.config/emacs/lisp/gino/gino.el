30e9c689c6c5a351f9ccaed116e758d8c967c7f2
Nicolas De Jaeghere <nicolas@dejaeghe.re>
2020-09-25 09:27:33 +0200

    [emacs] Update library headers

--- stow/emacs/.config/emacs/lisp/gino/gino.el
+++ stow/emacs/.config/emacs/lisp/gino/gino.el
@@ -1,5 +1,26 @@
 ;;; gino.el --- Generate Arduino project -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2020 Nicolas De Jaeghere
+
+;; Author: Nicolas De Jaeghere <nicolas@dejaeghe.re>
+;; Keywords: tools
+;; URL: https://github.com/Koekelas/dotfiles
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 
 ;; Generate an Arduino project backed by Meson
